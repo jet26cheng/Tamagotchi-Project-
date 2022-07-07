9f65fb967ff4e068932ff5b0a386bf3e6655745a
chengu <jet_cheng@yahoo.com>
2022-07-06 21:24:47 -0700

    Class

---
+++ js/app.js
@@ -0,0 +1,26 @@
+
+// make a tamagotchi class
+class tamagotchi {
+    constructor(Hunger,Sleepiness, Boredom, Age) {
+        this.Age = Age
+        this.Boredom = Boredom
+        this.Hunger = Hunger
+        this.Sleepiness = Sleepiness
+    }
+}
+// make a game object 
+const gameObject = { 
+    
+}
+
+//select each buttons and save them to a variable
+
+
+//add event listeners to the buttons 
+
+
+//look up set interval to start timer 
+//look up how to stop timer 
+
+
+//css animations 
