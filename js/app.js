115049ced51e05eff6b04576c4e552f25ae59e88
chengu <jet_cheng@yahoo.com>
2022-07-08 23:27:56 -0700

    variables

--- js/app.js
+++ js/app.js
@@ -1,18 +1,36 @@
 
 // make a tamagotchi class
-class tamagotchi {
-    constructor(Hunger,Sleepiness, Boredom, Age) {
-        this.Age = Age
-        this.Boredom = Boredom
-        this.Hunger = Hunger
-        this.Sleepiness = Sleepiness
+class Tamagotchi {
+    constructor(hunger,sleepiness, boredom, age, name) {
+        this.age = age
+        this.boredom = boredom
+        this.hunger = hunger
+        this.sleepiness = sleepiness
+        this.name = name
     }
 }
 // make a game object 
-const gameObject = { 
-    
+class Game { 
+    constructor() {
+        this.tamagotchi = new Tamagotchi(0, 0, 0, 0, "");
+
+    }
+    // petTamagotchi() {
+
+    // }
+    //this.tamagotchi.boredom-=1 
+
+    this.tamogachi.name = 
+nameTamogachi()
+nameTamogachi(name) {
+this.tamiogachi.name = name;
 }
 
+x = new Game()
+x.nameTamogachi(Jeddy);
+}
+
+const nameSpace = document.querySelector("#nameSpace")
 //select each buttons and save them to a variable
 
 
@@ -24,3 +42,31 @@ const gameObject = {
 
 
 //css animations 
+
+
+
+//game object creat functions for the game object to interact with the tamagotchi object. 
+// game object will control hunger sleep boredom etc. 
+
+// i want a live function() to start the timers of boredom, hunger, etc. - run function 
+
+//getHungry()
+//getSleepy()
+//getBored()
+
+// setinterval() functions that will call the game object functions that will make the tamagotchi bore, hungry tired, etc. 
+
+// these functions are controling the buttons that will help you pet, feed, sleep your tmaamgotchi 
+// pet()
+// lightsOff()
+// feed()
+
+// live functions is what is controling the buttons 
+// live ()
+
+on
+onClick={nameTamocachi(input)}
+
+run()
+
+setInterval( )
